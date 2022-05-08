bcaea40de6ceaa0c24e55883062d9e07c352e17b
edwin heras <djerizooriginal@gmail.com>
2022-05-07 19:52:31 -0500

    entrada y salida de comentarios

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,13 @@
 import { logging, PersistentUnorderedMap } from "near-sdk-as";
 export const comentarios = new PersistentUnorderedMap<string, string>("COMENTARIOS");
 
+// escitura de comentarios
+export function setComentario(id: string, TituloComentario: string): void {
+    comentarios.set(id, TituloComentario);
+}
 
+//lectura de comentarios
+export function getComentario(id: string): string | null {
+    return comentarios.get(id);
+}
 
