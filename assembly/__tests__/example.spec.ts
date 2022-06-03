251aa50883d3bdc0a5c1b9e4605c667f2fe48a6d
wiwis <djerizooriginal@gmail.com>
2022-06-03 18:08:04 -0500

    Creacion de test

---
+++ assembly/__tests__/example.spec.ts
@@ -0,0 +1,32 @@
+
+import { logging, PersistentUnorderedMap, u128} from "near-sdk-as";
+import { Comentario, comentariosTotal } from "../model";
+import {SetComentario, getComentarios, getComentario, comentarioLenght, EliminarComentario} from ".."
+const comentario = 'Bonito lugar';
+const Lugar_comentario = 'Chalcatongo';
+const Titulo = 'Recomiendo este lugar';
+
+const comentarioIndexx = comentariosTotal.length;
+let ComentarioNuevo = new Comentario(comentario, Lugar_comentario,Titulo);
+
+
+const data = new Array<Comentario>(comentarioIndexx);
+for(let i=0; i < comentarioIndexx; i++) {
+    data[i] = comentariosTotal[i]
+}
+
+describe("Subir Comentario", () => {
+  it('Si el nuevo comentario que se sube es el correcto', () => {
+      expect(SetComentario('Bonito lugar', 'Chalcatongo', 'Recomiendo este lugar')).toStrictEqual(ComentarioNuevo);
+  })
+})
+
+
+
+
+describe("getBooks", () => {
+  it('should return all books', () => {
+      expect(getComentarios()).toStrictEqual(data)
+  })
+})
+
